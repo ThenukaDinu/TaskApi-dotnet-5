7175593a97a2426093cbd1b984deef0f911d5021
Thenukad Dinu <thenukadinu@gmail.com>
2021-08-07 18:00:03 +0530

    Add project files.

---
+++ TaskApi/Controllers/TasksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new string[] { "Task1", "Task2", "Task3" };
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult NewTasks()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            var somethingWentWrong = true;
+            if (somethingWentWrong)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
