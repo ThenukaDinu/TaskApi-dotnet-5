0f9b32bd64f3c7384c2a131c9c02c3d0a88227bd
Thenukad Dinu <thenukadinu@gmail.com>
2021-08-07 18:52:19 +0530

    Create and separated molals and services

---
+++ TaskApi/Controllers/TodosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskApi.Modals;
+using TaskApi.Services;
+
+namespace TaskApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private TodoService _todoService;
+        public TodosController()
+        {
+            _todoService = new TodoService();
+        }
+
+        [HttpGet("{id?}")]
+        public IActionResult GetTodos (int? id)
+        {
+            var myTodos = _todoService.AllTodos();
+            if (id is null) return Ok(myTodos);
+
+            myTodos = myTodos.Where(t => t.Id == id).ToList();
+
+            return Ok(myTodos);
+        }
+    }
+}
